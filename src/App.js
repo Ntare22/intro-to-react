f9eb25ca32e4b2ed8b88b86c49cdb226c164799a
Jim Ntare <jim.ntare@gmail.com>
2020-04-09 09:44:52 +0200

    I learned about the different lifecycle components to use in my components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import MainContent from './components/MainContent'
+import React, { Component } from 'react';
 
-const App = () => {
-  return ( 
-    <div>
-      <NavBar />
-      <MainContent />
-      {/* <Footer /> */}
-    </div>
-  )
+class App extends Component {
+  state = {  }
+
+  componentDidMount() {
+    // This is usually where the api Call goes
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    // return new and updated state based on props
+  }
+
+  getSnapshotBeforeUpdate() {
+    // create a backup of the way current things are
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // return true if want it to update
+    // return false if not
+  }
+
+  componentWillUnmount() {
+    // this is where you tear down or cleanup your code before your component dissappears
+  }
+
+  render() { 
+    return ( 
+      <div>
+        Code goes here!
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
